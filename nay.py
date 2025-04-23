44f41eab04723f62dceda9bae6d4a5c5266e73f4
JeisonOlaya <75368622+JeisonOlaya@users.noreply.github.com>
2025-04-23 18:59:57 -0500

    Add files via upload

---
+++ nay.py
@@ -0,0 +1,57 @@
+#Nombre, apellido, correo y edad
+usuarios = []
+
+Menu = True
+
+while Menu:      
+    print("=="*20,"\n")
+    print("Bienvenido a la base de datos de estudiante \nActualmentete presentamos nuestro menu de opciones.\n")
+    print("=="*20,"\n")
+    
+    print("1.) Crear Usuario")
+    print("2.) Lista de Usuario")
+    print("3.) Editar Usuarios")
+    print("4.) Eliminar un Usuario\n")
+
+    escoger = int (input ("Que opcion elijes: "))
+    if escoger == 1:
+        continuar = True
+        while continuar: 
+            nombre = input("ingresa el nombre: ")
+
+            apellido = input("por favor ingresa el apellido: ")
+
+
+            correo = input("por favor ingrese el correo: ")
+
+
+            edad = input("por favor ingresa la edad: ")
+            usuarios.append([nombre,apellido,correo,edad])
+
+        
+            valor = input(" desea continuar agregando nombres: S(si) N(no)?: ")
+            if(valor == "N"):
+                continuar = False
+        
+        print("haz creado el usuario correctamente")
+        
+
+        volver = int(input("para volver al menu anterior, digita 1, o 2 para finalizar: "))
+        if volver==2:
+            print ("exitoso ")
+            Menu == False
+
+    if escoger == 2:
+        print(f"la Lista de usuario es: {print(usuarios)}")
+        
+
+if volver == 1: 
+    print("1.) Crear Usuario")
+    print("2.) Lista de Usuario")
+    print("3.) Editar Usuarios")
+    print("4.) Eliminar un Usuario\n")
+
+
+    
+
+    
